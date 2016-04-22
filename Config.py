b09910d6493ad1aefda2d097a067cb570e216f1f
Jose Canciani <jose.canciani@avature.net>
2016-04-22 18:46:21 -0300

    Add Config class to store requirements for our tools

---
+++ Config.py
@@ -0,0 +1,14 @@
+
+from file.File import fromFile, FileDoesNotExists, LanguageNotSupported
+
+class Config(object):
+    def __init__(self, classToFileCallback):
+        self.classToFileCallback = classToFileCallback
+    def getFileForClassName(self, className, namespace):
+        filePath = self.classToFileCallback(className, namespace)
+        if filePath:
+            try:
+                return fromFile(filePath)
+            except (FileDoesNotExists, LanguageNotSupported):
+                pass
+        return None
