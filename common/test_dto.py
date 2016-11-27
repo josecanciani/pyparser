3948b3af64264c7845f724eeb8debda9190fe80d
Jose Canciani <jose.canciani@avature.net>
2016-11-27 14:24:59 -0300

    Add base DTO object class to easily construct dto objects

---
+++ common/test_dto.py
@@ -0,0 +1,28 @@
+
+import unittest
+from common.dto import Dto
+
+class TestClassDto(unittest.TestCase):
+    def _getTestDto(self):
+        attrs = {
+            'myfirstattr': 'hola',
+            'mysecondattr': 10
+        }
+        return Dto(attrs)
+
+    def test_settersAndGetters(self):
+        dto = self._getTestDto()
+        self.assertEqual('hola', dto.myfirstattr)
+        self.assertEqual(10, dto.mysecondattr)
+
+    def test_invalidKey(self):
+        dto = self._getTestDto()
+        def okcallback():
+            return dto.myfirstattr
+        def errorcallback():
+            return dto.invalidkey
+        self.assertEqual('hola', okcallback())
+        self.assertRaises(KeyError, errorcallback)
+
+if __name__ == '__main__':
+    unittest.main()
